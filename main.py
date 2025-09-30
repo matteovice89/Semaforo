275891b5a870db3651edc6c1471a24426acb2752
Matteo Vicentini <145768623+matteovice89@users.noreply.github.com>
2025-09-30 16:57:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-class semaforo()#classe che va a definire i semafori per iniziare nord sud est ovest
-    def __init__(self, nome,verde,giallo,rosso,sensore):
+class semaforo():#classe che va a definire i semafori per iniziare nord sud est ovest
+    def __init__(self, nome,verde,giallo,rosso,sensore):#colori sono true o false
         self.nome=nome
         self.verde=verde
         self.giallo=giallo
@@ -12,10 +12,14 @@ class semaforo()#classe che va a definire i semafori per iniziare nord sud est o
     def stato(self): #metodo gestisce cambio di stato delle luci
         pass
 
-class strada()#classe per la gestione delle strade. in base a come voglio regolare il traffico
+class strada():#classe per la gestione delle strade. in base a come voglio regolare il traffico
               #vado a gestire il semforo
-    def __init__(self, nome, pedonale,scorre)#pedonale e scorre sono boolean
+    def __init__(self, nome, pedonale,scorre):#pedonale e scorre sono boolean
         self.nome=nome
         self.pedolale=pedonale
         self.scorre=scorre
-
+    
+semaforo_nord=semaforo('nord',False,False,False,False)
+semaforo_sud=semaforo('sud',False,False,False,False)
+semaforo_ovest=semaforo('ovest',False,False,False,False)
+semaforo_est=semaforo('est',False,False,False,False)
